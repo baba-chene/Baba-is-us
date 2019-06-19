36481a03be0f4d4a8a51979eb13a60789762861d
Weill--Duflos Colin <colin.weill--duflos@telecom-paristech.fr>
2019-06-19 11:36:17 +0200

    Add source of the event, set in the event controller

--- core/src/com/babachene/controller/ServerEventController.java
+++ core/src/com/babachene/controller/ServerEventController.java
@@ -42,11 +42,15 @@ public class ServerEventController {
     private void fetchEvents() {
         if(!server.isEventBufferEmpty()) {
     		LOGGER.fine("[Server Event Controller] Event received from server");
-            addEvent(server.getEvent());
+            Event event = server.getEvent();
+    		event.setSource(1);
+            addEvent(event);
         }
         if(eventGiver.size() > 0) {
     		LOGGER.fine("[Server Event Controller] Event received from input processor");
-            addEvent(eventGiver.pollEvent());
+    		Event event = eventGiver.pollEvent();
+    		event.setSource(0);
+            addEvent(event);
         }
     }
 
