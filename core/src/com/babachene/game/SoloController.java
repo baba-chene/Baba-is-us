2fa4bb1d20b662ea5d38ee1cfd75382560c28002
Guillaume <guillaume.demarles@telecom-paristech.fr>
2019-06-22 11:25:22 +0200

    Modified so that we can play the levels from the editor

--- core/src/com/babachene/game/SoloController.java
+++ core/src/com/babachene/game/SoloController.java
@@ -1,10 +1,13 @@
 package com.babachene.game;
 
+import java.io.IOException;
+
 import com.babachene.cliserv.InputEvent;
 import com.babachene.gui.LevelState;
 import com.babachene.gui.MainGame;
 import com.babachene.logic.GameLogic;
 import com.babachene.logic.data.LevelMap;
+import com.babachene.logic.data.MapEditorConverter;
 import com.babachene.userinput.EventGiver;
 import com.babachene.userinput.KeyboardMap;
 import com.babachene.userinput.LevelInputProcessor;
@@ -39,21 +42,14 @@ public class SoloController extends Controller {
 		 * TEST ZONE : Create a custom map for testing.
 		 */
 		
-		LevelMap map = new LevelMap(10, 10);
-		
-		map.addEntity(2, 4, "baba");
-		map.addEntity(5, 5, "textis");
-		map.addEntity(5, 8, "textbaba");
-		map.addEntity(4, 2, "rock");
-		map.addEntity(5, 2, "textrock");
-		map.addEntity(5, 4, "textpush");
-		map.addEntity(5, 7, "textsink");
-		map.addEntity(1, 1, "textbaba");
-		map.addEntity(2, 1, "textis");
-		map.addEntity(3, 1, "textyou");
-		map.addEntity(6, 5, "textyou");
-		map.addEntity(6, 6, "textrock");
-		
+		MapEditorConverter mapEditorConverter = new MapEditorConverter(30, 20);
+		try {
+			mapEditorConverter.open("C:\\Users\\Guillaume\\Desktop\\level1.txt\\");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		LevelMap map = mapEditorConverter.getMap();
 		logic = new GameLogic(map);
 		
 		mainGame.push(new LevelState(map, inputProcessor));
