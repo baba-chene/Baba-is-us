c8313b6de8f9a2e69a516c20295b98cabe484128
Jeremy Kaplan <jeremy.kaplan@telecom-paristech.fr>
2019-06-24 17:42:39 +0200

    WIP input shall send events at time intervals

--- core/src/com/babachene/userinput/LevelInputProcessor.java
+++ core/src/com/babachene/userinput/LevelInputProcessor.java
@@ -1,6 +1,8 @@
 package com.babachene.userinput;
 
+import com.babachene.Baba;
 import com.babachene.cliserv.InputEvent;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 
 /**
@@ -14,22 +16,44 @@ import com.badlogic.gdx.InputAdapter;
  * However, this is for later, once we managed the movement speed.
  */
 public class LevelInputProcessor extends InputAdapter {
+	/** No comment */
+	public static final float DURATION = 23f / Baba.BASE_ENTITY_MOVING_SPEED;
 	
+	
+	/** If this boolean is true, then player is taken into account. Otherwise no. */
+	private boolean soloOn;
+	/** The keyboard mapping. */
 	private KeyboardMap keymap;
 	/** The EventGiver to actually offer the events to.*/
 	private EventGiver eventGiver;
 	/** flag for insuring the event methods return the correct boolean. */
 	private boolean flag = false;
 	/** For remembering if the key was pressed or released when an event occurs on it. */
-	private boolean zRequestDown = false, resetRequestDown = false;
+	private boolean
+			move1Down = false,
+			move2Down = false,
+//			waitDown = false, // wait is not implemented in the logic yet.
+			zRequestDown = false,
+			resetRequestDown = false;
+	/** For counting time. */
+	private float
+			move1Time = -1,
+			move2Time = -1;
+	private InputEvent
+			move1Event,
+			move2Event;
 	
-	public LevelInputProcessor(KeyboardMap km, EventGiver eventGiver) {
+	public LevelInputProcessor(KeyboardMap km, EventGiver eventGiver, boolean isSolo) {
 		if (km == null)
 			throw new IllegalArgumentException("KeyboardMap is null for LevelInputProcessor.");
 		keymap = km;
 		if (eventGiver == null)
 			throw new IllegalArgumentException("EventGiver is null for LevelInputProcessor.");
 		this.eventGiver = eventGiver;
+		soloOn = isSolo;
+	}
+	public LevelInputProcessor(KeyboardMap km, EventGiver eventGiver) {
+		this (km, eventGiver, false);
 	}
 	
 	///////////////////////////////
@@ -47,6 +71,19 @@ public class LevelInputProcessor extends InputAdapter {
 		return eventGiver;
 	}
 	
+	public boolean isSoloOn() {
+		return soloOn;
+	}
+	/**
+	 * Tells this input processor whether it should send events
+	 * for both player 1 and 2 (one keyboard mode) or just player 1.
+	 * @param isSoloOn
+	 */
+	public void setSoloMode(boolean isSoloOn) {
+		soloOn = isSoloOn;
+		move2Down = false;
+	}
+	
 	///////////////////////////////
 	
 	@Override
@@ -58,39 +95,45 @@ public class LevelInputProcessor extends InputAdapter {
 		 * I do know this is ugly...
 		 */
 		if (keycode == keymap.GO_UP) {
-			eventGiver.addEvent(new InputEvent(InputEvent.MOVE_UP, (byte)1));
-			flag = true;
+			move1Event = (new InputEvent(InputEvent.MOVE_UP, (byte)1));
+			move1Down = flag = true;
 		}
 		if (keycode == keymap.GO_DOWN) {
-			eventGiver.addEvent(new InputEvent(InputEvent.MOVE_DOWN, (byte)1));
-			flag = true;
+			move1Event = (new InputEvent(InputEvent.MOVE_DOWN, (byte)1));
+			move1Down = flag = true;
 		}
 		if (keycode == keymap.GO_LEFT) {
-			eventGiver.addEvent(new InputEvent(InputEvent.MOVE_LEFT, (byte)1));
-			flag = true;
+			move1Event = (new InputEvent(InputEvent.MOVE_LEFT, (byte)1));
+			move1Down = flag = true;
 		}
 		if (keycode == keymap.GO_RIGHT) {
-			eventGiver.addEvent(new InputEvent(InputEvent.MOVE_RIGHT, (byte)1));
-			flag = true;
+			move1Event = (new InputEvent(InputEvent.MOVE_RIGHT, (byte)1));
+			move1Down = flag = true;
 		}
-		if (keycode == keymap.GO_UP_2) {
-			eventGiver.addEvent(new InputEvent(InputEvent.MOVE_UP, (byte)2));
-			flag = true;
+		/*
+		 * Event from player 2.
+		 */
+		if (soloOn) {
+			if (keycode == keymap.GO_UP_2) {
+				move2Event = (new InputEvent(InputEvent.MOVE_UP, (byte)2));
+				move2Down = flag = true;
+			}
+			if (keycode == keymap.GO_DOWN_2) {
+				move2Event = (new InputEvent(InputEvent.MOVE_DOWN, (byte)2));
+				move2Down = flag = true;
+			}
+			if (keycode == keymap.GO_LEFT_2) {
+				move2Event = (new InputEvent(InputEvent.MOVE_LEFT, (byte)2));
+				move2Down = flag = true;
+			}
+			if (keycode == keymap.GO_RIGHT_2) {
+				move2Event = (new InputEvent(InputEvent.MOVE_RIGHT, (byte)2));
+				move2Down = flag = true;
+			}
 		}
-		if (keycode == keymap.GO_DOWN_2) {
-			eventGiver.addEvent(new InputEvent(InputEvent.MOVE_DOWN, (byte)2));
-			flag = true;
-		}
-		if (keycode == keymap.GO_LEFT_2) {
-			eventGiver.addEvent(new InputEvent(InputEvent.MOVE_LEFT, (byte)2));
-			flag = true;
-		}
-		if (keycode == keymap.GO_RIGHT_2) {
-			eventGiver.addEvent(new InputEvent(InputEvent.MOVE_RIGHT, (byte)2));
-			flag = true;
-		}
-		
-		// The request event.
+		/*
+		 *  The request event.
+		 */
 		if (keycode == keymap.Z_REQUEST) {
 			if (zRequestDown == false) {
 				zRequestDown = true;
@@ -129,6 +172,24 @@ public class LevelInputProcessor extends InputAdapter {
 		}
 		
 		return flag;
+	}
+	
+	/**
+	 * Update this input processor. This method is necessary for the input
+	 * processor is at charge for sending move events at regular time intervals in
+	 * case the player keeps a key down to move.
+	 */
+	public void update() {
+		if (move1Time > 0) {
+			move1Time -= Gdx.graphics.getDeltaTime();
+		}
+		if (move1Down) {
+			if (move1Time > 0) { // Here the event shall not be sent now.
+				
+			} else { // There we send the event and reset the time counter.
+				eventGiver.addEvent(move1Event);
+			}
+		}
 	}
 	
 }
