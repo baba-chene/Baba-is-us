7b0270ab2aa335d37355535c25c367873c4110df
Jeremy Kaplan <jeremy.kaplan@telecom-paristech.fr>
2019-06-24 10:34:23 +0200

    WIP add tools for rendering a directional entity

--- core/src/com/babachene/logic/data/Direction.java
+++ core/src/com/babachene/logic/data/Direction.java
@@ -1,5 +1,11 @@
 package com.babachene.logic.data;
 
+/**
+ * Represents the fourth direction an entity can
+ * face to.
+ * @author guillaume & jeremy
+ *
+ */
 public enum Direction {
 		NORTH,
 		SOUTH,
